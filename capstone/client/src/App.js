d3a6fc65ee8535550facaf161eac42f9445cf49b
Conner Montgomery <cmontgomery2799@gmail.com>
2022-10-08 16:52:21 -0400

    simple login system

--- capstone/client/src/App.js
+++ capstone/client/src/App.js
@@ -1,36 +1,111 @@
+//Must install in client:
+// React
+// Axios - simple way to replicating http fetch function request
 
-import React from "react";
+
+
+import React, {useState} from "react";
 import logo from "./logo.svg";
+import Axios from 'axios';
 import "./App.css";
+
 
 function App() {
   const [data, setData] = React.useState(null);
+
+  // States for Registration
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+	
+  // States for log in 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+	
+  // State for feedback on login status 
+  const [loginStatus, setLoginStatus] = useState("");
+  
+  // To put registration info and send request to back-end to insert user into DB
+  const register = () => {
+	  Axios.post('http://localhost:3001/register', {
+		  username:usernameReg, 
+		  password:passwordReg,
+	  }).then((response)=> {
+		  console.log(response);
+	  });
+  };
+	
+	
+  // To put login info and send request to back-end to select user from DB
+  const login = () => {
+	  Axios.post('http://localhost:3001/login', {
+		  username:username,
+		  password:password,
+	  }).then((response)=> {
+		  if(response.data.message) {
+			  setLoginStatus(response.data.message);
+		  } else {
+			  setLoginStatus(response.data[0].email);
+		  }
+		  
+		  console.log(response);
+		  
+	  });
+  };
 
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
+	
+	
+
 
   return (
+	  
+	  
+	  
 	  
 	  <div className="App">
 	  	<div className="registration">
 	  		<h1>Registration</h1>
 	  		<label>Username</label>
-	  		<input type="text" />
+	  		<input type="text" 
+	  		onChange= {(e)=> {
+	  				setUsernameReg(e.target.value);
+  			}}
+			/>
 	  		<label>Password</label>
-	  		<input type="text" />
-	  		<button> Register </button>
+	  		<input type="text" 
+			onChange= {(e)=> {
+	  				setPasswordReg(e.target.value);
+  			}}
+			
+			/>
+	  		<button onClick= { register }> Register </button>
 	  	</div>
 	  
 	  
 	  	<div className="login">
-	  		<h1>Registration</h1>
-	  		<input type="text" placeholder="Username..."  />
-	  		<input type="password" placeholder="Password..." />
-	  		<button>Register</button>
+	  		<h1>Log In</h1>
+	  		<input type="text" placeholder="Username..."  
+			onChange= {(e)=> {
+				setUsername(e.target.value);
+			}}
+			/>
+	  		<input type="password" placeholder="Password..."
+			onChange= {(e)=> {
+				setPassword(e.target.value);	   
+			}}
+			
+			
+			/>
+	  		<button onClick= { login }>Register</button>
 	  	</div>
+			
+			<h1>{ loginStatus }</h1>
+			
+			
 	  </div>
 	  
 	  
--- capstone/server/index.js
+++ capstone/server/index.js
@@ -1,14 +1,71 @@
- const express = require("express");
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+
+const db = mysql.createConnection({
+	user: "root",
+	host: "localhost",
+	password: "Capstone$$",
+	database: "CapstoneProject",
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.post("/register", (req, res) => {
+	
+	
+	const username = req.body.username;
+	const password = req.body.password;
+	
+	
+	
+	db.query("INSERT INTO users (email, password) VALUES (?,?)", 
+			 [username, password], 
+			 (err, result)=> {
+				console.log(err);
+				
+			}
+	);
+});
+
+
+
+app.post("/login", (req, res) => {
+	
+	const username = req.body.username;
+	const password = req.body.password;
+	
+	db.query("SELECT * FROM users WHERE email = ? AND password = ?",
+		[username, password],
+		(err, result)=> {
+		if(err) {
+			res.send({err: err});
+		} 
+		
+		
+		if(result.length > 0) {
+			res.send(result);
+		} else {
+			res.send({message: "Wrong user/pass combination"});
+		}
+		}
+	);
+	
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
