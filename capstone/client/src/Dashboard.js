6a71c7c86590174f6fca20118c66360846a20705
Conner Montgomery <cmontgomery2799@gmail.com>
2022-10-23 16:05:01 -0400

    dashboard

--- capstone/client/src/Dashboard.js
+++ capstone/client/src/Dashboard.js
@@ -1,11 +1,40 @@
 
+import "./dashboard.css";
+import "./materialize.css";
+
 function Dashboard() {
 	return(
 	
-	<div>
-			<h1>Dashboard!</h1>
+<div class="container">
+	<div class="row dash">
+		
+		<div id="leftColumn" class="col s4">
+			<h5>KENT STATE PARKING</h5>
+			<h6>Spaces Available</h6>
+			
+			<p id="count">335</p>
+		
+			<p id="lotName">Lot: MSB (R-1)</p>
+		
+		</div>
+	
+		<div class="col s8 rightColumn">
+			
+			<p id="lotMenu">MSB</p>
+			<p id="spacesAvailable">SPACES AVAILABLE</p>
+			<div class="analytics">
+				<p id="time">12:00pm</p>	
+				<p>The lot is typically busy at this time.</p>	
+			</div>
+		</div>
 		
 	</div>
+	<div class="row adminControls">
+		<div class="col s12 admin">
+			<p>Put admin controls here</p>
+		</div>
+	</div>
+</div>
 	
 	);
 		
